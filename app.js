fb0bf6f819d8861ae922c6ba450d649db8cdbcfc
Sid Malik <siddharthmalik123@gmail.com>
2016-09-14 12:42:53 -0400

    port settings

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ var app = express();
 
 app.use('/static', express.static(path.join(__dirname , '/public')))
 
+app.listen(process.env.PORT || 3005, function(){
+	console.log('mock-users.com is running')
+})
+
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '/public', '/html', 'mockuser.html'))
 })
 
-app.listen(process.env.PORT || 3005, function(){
-	console.log('mock-users.com is running')
-})
